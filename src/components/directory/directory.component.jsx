c5922a47fbf11e5c0544f80e9e292da80e8e7a90
adampatt <adampattison08@gmail.com>
2020-12-18 13:31:51 +0000

    Changed capitalisation of imageURL to imageUrl to get it working

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,8 +47,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageURL, id}) => (
-                        <MenuItem key={id} title={title} imageURL={imageURL}/>
+                    this.state.sections.map(({title, imageUrl, id, size}) => (
+                        <MenuItem key={id} title={title} imageURL={imageUrl} size={size}/>
                     ) )
                  } 
                 

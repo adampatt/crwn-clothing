d0794f30d061e3fa37e2053e05a893b5211e1cf4
adampatt <adampattison08@gmail.com>
2020-12-18 13:21:55 +0000

    added images to the back of the div

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,7 +48,7 @@ class Directory extends React.Component {
             <div className="directory-menu">
                 {
                     this.state.sections.map(({title, imageURL, id}) => (
-                        <MenuItem key={id} title={title} />
+                        <MenuItem key={id} title={title} imageURL={imageURL}/>
                     ) )
                  } 
                 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => (
-  <div className="menu-item">
+const MenuItem = ({ title, imageURL }) => (
+  <div 
+  style={{
+    backgroundImage: `url(${imageURL})`
+  }}
+  className="menu-item">
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">SHOP NOW</span>

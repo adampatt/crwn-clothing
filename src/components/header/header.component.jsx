775ace7f950115e9b2e474359a07af8caf31224b
adampatt <adampattison08@gmail.com>
2021-01-11 13:17:17 +0100

    created head component and stylesheet

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.styles.scss';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+    <div className="header">
+        <Link to="/">
+            
+        </Link>
+    </div>
+)
+
+export default Header;

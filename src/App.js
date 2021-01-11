cbac9fd1813b2e44acb72371bc2612b7095b1f75
adampatt <adampattison08@gmail.com>
2021-01-11 10:53:17 +0100

    76 was a demo video

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import { Switch, Route } from 'react-router-dom'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
+import HomePage from "./pages/homepage/homepage.component";
 
 const HatsPage = () => (
   <div>
-    <h1>Hats Page</h1>
+    <h1>HATS PAGE </h1>
   </div>
 );
-
 
 function App() {
   return (

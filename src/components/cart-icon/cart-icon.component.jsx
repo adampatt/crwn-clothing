33336924b7c74c6c8729f40c29ef47ea2d53eac1
adampatt <adampattison08@gmail.com>
2021-01-18 15:50:00 +0100

    typo

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,8 +6,8 @@ import { toggleCartHidden }  from '../../redux/cart/cart.actions'
 
 
 
-const CartIcon = () => (
-    <div className="cart-icon">
+const CartIcon = ({ toggleCartHidden }) => (
+    <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
         <span className="item-count"> 0</span>
     </div>

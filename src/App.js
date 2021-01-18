0be8f670cea1c597eef4d47e3f8817f3ea474fd2
adampatt <adampattison08@gmail.com>
2021-01-18 13:56:25 +0100

    setting up redux flow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import "./App.css";
 import ShopPage from "./pages/shop/shop.component";
@@ -52,7 +52,11 @@ class App extends React.Component  {
 }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null, mapDispatchToProps )(App);
+export default connect(mapStateToProps, mapDispatchToProps )(App);

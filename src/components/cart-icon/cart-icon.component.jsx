a58a271f48eb828d41eeae111fdade6cde2862fe
adampatt <adampattison08@gmail.com>
2021-01-18 15:47:32 +0100

    imported the cart-action into the cart icon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { connect } from 'react-redux';
+import { toggleCartHidden }  from '../../redux/cart/cart.actions'
+
+
 
 const CartIcon = () => (
     <div className="cart-icon">

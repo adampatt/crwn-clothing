6dbf92723b7676b4dff9e5d80f59b2ada767298e
adampatt <adampattison08@gmail.com>
2021-01-25 09:45:33 +0100

    imported addItem to collection-item component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-
+import { connect } from "react-redux";
 import './collection-item.styles.scss';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import CustomButton from '../custom-button/custom-button.component';
 

88202aefd249e9b3fc85866865ce0f43d2e0c0b4
adampatt <adampattison08@gmail.com>
2021-01-25 10:14:06 +0100

    Cart now updates when you click add item to cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -8,21 +8,25 @@ import CustomButton from '../custom-button/custom-button.component';
 
 
 
-const CollectionItem = ({ id, name, price, imageUrl, addItem }) => (
-    <div className='collection-item'>
-        <div
-            className="image"
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className="collection-footer">
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageURL } = item;
+    return (
+        <div className='collection-item'>
+            <div
+                className="image"
+                style={{
+                    backgroundImage: 'url(${imageURL})'
+                }}
+            />
+            <div className="collection-footer">
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            <CustomButton onClick={() => addItem(item)} inverted> Add to cart </CustomButton>
         </div>
-        <CustomButton inverted> Add to cart </CustomButton>
-    </div>
-);
+    )
+};
+
 
 const mapDispatchToProps = dispatch => ({
     addItem: item => dispatch(addItem(item))

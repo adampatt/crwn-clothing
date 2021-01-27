1cc1969064031209c381eba98b1c14022603e1fe
adampatt <adampattison08@gmail.com>
2021-01-27 10:27:10 +0100

    page layout created and UTF8 X inserted

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './checkout-item.styles.scss';
+
+const CheckoutItem = () => (
+    <div className="checkout-item">
+        <div className="image-container">
+            <img alt="item" />
+        </div>
+        <span className="name"></span>
+        <span className="quantity"></span>
+        <span className="price"></span>
+        <span className="remove-button">&#10005;</span>
+    </div>
+)
+
+export default CheckoutItem;

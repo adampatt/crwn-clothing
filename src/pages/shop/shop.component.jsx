d2dbdd0f061a27f4450705a660815b23832ceb4e
adampatt <adampattison08@gmail.com>
2021-01-28 16:46:45 +0100

    routes set up to be ableto pass category id dynamically into selector

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route } from "react-router-dom";
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-
+import CategoryPage from '../category/category.component';
 
 const ShopPage = ({ match }) => (
     <div className='shop-page'>
         <Route exact path={`${match.path}`}component={CollectionsOverview} />
-    </div>);
+    </div>
+    );
 
 
 export default ShopPage;

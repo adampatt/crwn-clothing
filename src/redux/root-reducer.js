acbebd893f35ee3a6bace8fec6d17c436bc4f060
adampatt <adampattison08@gmail.com>
2021-01-28 11:22:17 +0100

    index.js app wrapped in persist gate so the app has access all of the time to the new reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,7 +5,17 @@ import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
-    user: userReducer,
-    cart: cartReducer
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
+    
+

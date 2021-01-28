85bb7b3f1769cd2c8ca0bb09fe827cb267eb7f8f
adampatt <adampattison08@gmail.com>
2021-01-28 16:49:09 +0100

    endof lesson tidy up

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,10 +4,11 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CategoryPage from '../category/category.component';
 
 const ShopPage = ({ match }) => (
-    <div className='shop-page'>
-        <Route exact path={`${match.path}`}component={CollectionsOverview} />
-    </div>
-    );
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+  </div>
+);
 
 
 export default ShopPage;

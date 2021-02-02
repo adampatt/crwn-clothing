733f730d6abb25c8806713517c21e11f38482763
adampatt <adampattison08@gmail.com>
2021-02-02 14:02:01 +0100

    everything needed inside stripecheckout tag

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -4,4 +4,24 @@ import StripeCheckout from 'react-stripe-checkout';
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_S3SL5UCbLCkuGMo29jO9AOe4';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+      <StripeCheckout
+        label="Pay Now"
+        name="CRWN Clothing Ltd"
+        billingAddress
+        shippingAddress
+        image=""
+        description={`Your total is $${price}`}
+        amount={priceForStripe}
+        panelLabel="Pay now"
+        token={onToken}
+        stripeKey={publishableKey}
+      />
+    );
 }

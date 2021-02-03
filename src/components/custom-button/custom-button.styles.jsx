f7af5e6adc9e6818a49b260f90a3fd9309b0cf4f
adampatt <adampattison08@gmail.com>
2021-02-03 16:40:47 +0100

    styled-components sheet created

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -1,5 +1,17 @@
 import styled, { css } from "styled-components";
 
+const buttonStyles = css`
+  background-color: black;
+  color: white;
+  border: none;
+
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+  }
+`;
+
 const invertedButtonStyles = css`
     background-color: white;
     color: black;
@@ -18,6 +30,15 @@ const googleSignInStyles = css`
       background-color: #357ae8;
       border: none;`;
 
+
+const getButtonStyles = props => {
+    if (props.googleSign) {
+        return googleSignInStyles;
+    }
+
+    return props.inverted ? invertedButtonStyles : buttonStyles;
+}
+
 export const CustomButtonContainer = styled.button`
   min-width: 165px;
   width: auto;
@@ -26,19 +47,10 @@ export const CustomButtonContainer = styled.button`
   line-height: 50px;
   padding: 0 35px 0 35px;
   font-size: 15px;
-  background-color: black;
-  color: white;
   text-transform: uppercase;
   font-family: "Open Sans Condensed";
   font-weight: bolder;
-  border: none;
   cursor: pointer;
   display: flex;
   justify-content: center;
-
-  &:hover {
-    background-color: white;
-    color: black;
-    border: 1px solid black;
-  }
 `;

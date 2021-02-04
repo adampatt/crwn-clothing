d7f190d0aad2395f121d4bfd5311c61f76b22733
adampatt <adampattison08@gmail.com>
2021-02-04 16:45:44 +0100

    should be able to see consolelog of rebase data but not showing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { Route } from "react-router-dom";
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
+import { firestore } from '../../firebase/firebase.utils';
 
-class ShopPage extends React.component {
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+componentDidMount() {
+  const collectionRef = firestore.collection('collections');
+
+  collectionRef.onSnapshot(async snapshot => {
+    console.log(snapshot);
+  })
+}
+
   render() {
     const { match } = this.props;
      return (

15d1f53d2d812fcd8d9a8e5c5bcacb12bad5a848
adampatt <adampattison08@gmail.com>
2021-02-04 14:37:16 +0100

    the import is destructured to keep only the data we want to keep

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -42,10 +42,17 @@ const config = {
 
   firebase.initializeApp(config);
 
-  export const addCollectionAndDocuments = (collectionKey, objectsToAdd) => {
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
     const collectionRef= firestore.collection(collectionKey);
-    console.log(collectionRef)
-  }
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+  };
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();

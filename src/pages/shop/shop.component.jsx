b94fa46deb57acaa574d1c8e2d838eb93af8ae6e
adampatt <adampattison08@gmail.com>
2021-02-05 11:14:29 +0100

    inserted into the shop component for loading pages

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -9,9 +9,10 @@ import {
   convertCollectionsSnapshotToMap,
 } from "../../firebase/firebase.utils";
 
-import WithSpinnner from '../../components/with-spinner/with-spinner.component';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
-
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
   state = {
@@ -27,18 +28,22 @@ componentDidMount() {
   collectionRef.onSnapshot(async Snapshot => {
     const collectionsMap = convertCollectionsSnapshotToMap(Snapshot);
     updateCollections(collectionsMap);
-    this.setState({ loading: flase });
+    this.setState({ loading: false });
   });
 }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
      return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route exact path={`${match.path}`} 
+        render={(props) => <CollectionsOverviewWithSpinner 
+        isLoading={loading} {...props} />} />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+           render={(props) => <CollectionPageWithSpinner
+           isLoading={loading} {...props} />} />
         />
       </div>
     );

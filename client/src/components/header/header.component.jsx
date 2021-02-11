16baffddfe5b1ea5303089d3f2fc4f9e5911d8d1
adampatt <adampattison08@gmail.com>
2021-02-11 15:56:19 +0100

    remove gitignore files

---
+++ client/src/components/header/header.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
+import { createStructuredSelector } from "reselect";
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selector';
+
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from "./header.styles";
+
+const Header = ({ currentUser, hidden }) => (
+  <HeaderContainer>
+    <LogoContainer to="/">
+      <Logo className="logo" />
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to="/shop">SHOP</OptionLink>
+      <OptionLink to="/shop">CONTACT</OptionLink>
+      {currentUser ? (
+        <OptionDiv onClick={() => auth.signOut()}>
+          SIGN OUT{" "}
+        </OptionDiv>
+      ) : (
+        <OptionLink to="/signin"> SIGN IN</OptionLink>
+      )}
+      <CartIcon />
+    </OptionsContainer>
+    {hidden ? null : <CartDropdown />}
+  </HeaderContainer>
+);
+
+const mapStateToProps = createStructuredSelector ({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
+});
+
+export default connect(mapStateToProps)(Header);
---
+++ client/src/firebase/firebase.utils.js
@@ -0,0 +1,85 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyAfUnTwiohCWHF81RBt-XGUB-Gt0naFRso",
+    authDomain: "crwn-db-ff33a.firebaseapp.com",
+    projectId: "crwn-db-ff33a",
+    storageBucket: "crwn-db-ff33a.appspot.com",
+    messagingSenderId: "293088543967",
+    appId: "1:293088543967:web:10f942ad5aa2feb38c156a",
+    measurementId: "G-WW3Y79204X"
+  };
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+    
+    
+
+    if(!snapShot.exists) {
+        const { displayName, email }  = userAuth;
+        const createdAt = new Date();
+        
+        try {
+          await userRef.set({
+            displayName,
+            email,
+            createdAt,
+            ...additionalData
+          })
+        } catch (error) {
+          console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+  };
+
+  firebase.initializeApp(config);
+
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef= firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+  };
+
+  export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+      const { title, items } = doc.data();
+
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items
+      }
+    });
+    
+    return transformedCollection.reduce((accumulator, collection) => {
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    }, {});
+  };
+
+  
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
+
+  
